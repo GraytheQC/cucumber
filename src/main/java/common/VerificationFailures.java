d6613c01c78f4d10f4ad5f27ac56f2ce204d1edc
GraytheQC <62008505+GraytheQC@users.noreply.github.com>
2020-03-10 17:28:41 +0700

    Update VerificationFailures.java

--- src/main/java/common/VerificationFailures.java
+++ src/main/java/common/VerificationFailures.java
@@ -10,6 +10,7 @@ public class VerificationFailures extends HashMap<ITestResult, List<Throwable>> 
 	/**
 	*
 	*/
+//Bao cute
 	private static final long serialVersionUID = 1L;
 
 	public static VerificationFailures getFailures() {
